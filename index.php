39f9d32983361f637b4e2f9c0007dbac7418c0d1
Adam Elsodaney <adam.elso@gmail.com>
2014-03-19 00:11:31 +0000

    Update version constraint message

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 
 error_reporting(E_ALL);
-
 ini_set('xdebug.scream', 1);
 
 # let people know if they are running an unsupported version of PHP
-if (phpversion() < 5.3) {
+if ($phpVersion = phpversion() < 5.3) {
 
-  die(<<<'HTML'
-<h3>Stacey requires PHP/5.3 or higher.<br>You are currently running PHP/'.phpversion().'.</h3>
+    $message = <<<HTML
+<h3>Stacey requires PHP 5.3 or higher.<br>You are currently running PHP $phpVersion.</h3>
 <p>You should contact your host to see if they can upgrade your version of PHP.</p>'
-HTML
-);
+HTML;
+
+    exit($message);
 
 } else {
 
