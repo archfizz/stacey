819690e7b3429fcfb3f8dbdfcdd924b3242628f3
Adam Elsodaney <adam.elso@gmail.com>
2014-03-19 00:13:42 +0000

    Create development and production front ends

--- index.php
+++ index.php
@@ -1,26 +1,10 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('xdebug.scream', 1);
+$loader = require __DIR__.'/vendor/autoload.php';
 
-# let people know if they are running an unsupported version of PHP
-if ($phpVersion = phpversion() < 5.3) {
+require_once __DIR__.'/app/parsers/markdown-parser.inc.php';
 
-    $message = <<<HTML
-<h3>Stacey requires PHP 5.3 or higher.<br>You are currently running PHP $phpVersion.</h3>
-<p>You should contact your host to see if they can upgrade your version of PHP.</p>'
-HTML;
+$request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
 
-    exit($message);
-
-} else {
-
-    $loader = require __DIR__.'/vendor/autoload.php';
-
-    require_once __DIR__.'/app/parsers/markdown-parser.inc.php';
-
-    $request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
-
-    // start the app
-    new Stacey\Application($request->query->all());
-}
+// start the app
+new Stacey\Application($request->query->all());
